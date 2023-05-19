75a141bb90b06ccb3219907e7e4bc12970f43ac8
Sourav <ouravthedevguy@gmail.com>
2023-05-20 01:31:29 +0530

    Added redux store

--- frontend/appts1/src/main.tsx
+++ frontend/appts1/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './../router';
+import { Provider } from 'react-redux'
+import { store } from './store/store';
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div className='data-new-gr-c-s-check-loaded="14.1110.0'>
-      <RouterProvider router={router} />
-    </div>
+    <Provider store={store}>
+      <div className='data-new-gr-c-s-check-loaded="14.1110.0'>
+        <RouterProvider router={router} />
+      </div>
+    </Provider>
   </React.StrictMode>,
 )
